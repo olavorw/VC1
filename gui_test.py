edfcdb8173d35a1846c11f1f8d70986662cc9976
Olanorw <73853649+Olanorw@users.noreply.github.com>
2024-06-18 13:05:17 -0700

    Update image paths in gui_test.py

--- gui_test.py
+++ gui_test.py
@@ -9,7 +9,7 @@ SCREEN_HEIGHT = 900
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('VC1')
-icon_image = pygame.image.load('vc1.ico')
+icon_image = pygame.image.load('assets/logo/VC1.ico')
 pygame.display.set_icon(icon_image)
 
 font = pygame.font.SysFont("Tw Cen MT Condensed Extra Bold", 40)
@@ -21,7 +21,7 @@ def draw_text(text, font, text_col, x, y):
     img = font.render(text, True, text_col)
     screen.blit(img, (x, y))
     
-background = pygame.image.load('gui/bg.png')
+background = pygame.image.load('assets/gui/bg.png')
     
 run = True
 while run:
