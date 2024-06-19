888d6f393fc0939682248e6abba6ad5a94bc6190
Olanorw <73853649+Olanorw@users.noreply.github.com>
2024-06-19 10:11:56 -0700

    Refactor voice_handler.py to update API request message

--- testing/voice_handler.py
+++ testing/voice_handler.py
@@ -4,7 +4,7 @@ import os
 
 url = "https://api.elevenlabs.io/v2"
 
-headers = {"xi-api-key": "92bf014d900ce7ef6b1a83e7e864f9a8"}
+headers = {"xi-api-key": ""}
 
 response = requests.request("GET", url, headers=headers)
 
