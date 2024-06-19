43fb4c4bf64b0023febc9e02e42bf07fd0e9df92
Olanorw <73853649+Olanorw@users.noreply.github.com>
2024-06-19 10:09:59 -0700

    Testing voices

--- testing/voice_handler.py
+++ testing/voice_handler.py
@@ -1,8 +1,14 @@
 import requests
+import requests
+import os
 
 url = "https://api.elevenlabs.io/v1/voices"
 
-headers = {"xi-api-key": ""}
+headers = {"xi-api-key": "YOUR_API_KEY"}
+
+response = requests.request("GET", url, headers=headers)
+
+print(response.text)
 
 response = requests.request("GET", url, headers=headers)
 
